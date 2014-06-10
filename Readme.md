74ba16c3f3fbf06de27b8384bdebbf124fd7f241
Finn <derfinn@gmail.com>
2014-06-10 23:46:49 +0200

    hey

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# testscore
+
+Small cli for checking how many and which modules of a user have specified a 
+test field in their package.json. It ironically doesnt have tests.
+
+Install with `npm install testscore -g`.
+
+```
+$ testscore -h                                                                         
+
+Usage: node testscore [options]
+
+Options:
+   -a, --author   The author to show the tests from  [npm whoami]
+```
+
+Example video: https://asciinema.org/a/10071
---
+++ cli.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+var request = require('request');
+var colors = require('colors');
+var exec = require('sync-exec');
+
+var opts = require('nomnom')
+  .option('author', {
+    abbr: 'a',
+    default: 'npm whoami',
+    help: 'The author to show the tests from'
+  })
+  .parse()
+  ;
+  
+  // Default to npm whoami
+  if(opts.author !== 'npm whoami') {
+    var author = opts.author
+  } else {
+    var author = exec('npm whoami').stdout.trim();
+  }
+    
+  var nWithTests = 0;
+  var url = 'http://registry.npmjs.org/-/by-user/' + author;
+  console.log('Requesting modules by ' + author + ' ...');
+  request({url: url, json: true}, function (err, response, body) {
+    var packages = body[author];
+    packages.forEach(function (pack) {
+      process.stdout.write('- ' + pack);
+      checkTestField(pack);
+    });
+    console.log(nWithTests + '/' + packages.length + ' modules have tests');
+  });
+  
+  function checkTestField(pack) {
+    var testcommand =  exec('npm view ' + pack +' scripts.test').stdout.trim();
+    if(testcommand && testcommand != 'echo "Error: no test specified" && exit 1') {
+      process.stdout.write(' \u2713'.green);
+      nWithTests++;
+    } else {
+      process.stdout.write(' \u2717'.red);
+    }
+    console.log();
+  }
+
+  
+
